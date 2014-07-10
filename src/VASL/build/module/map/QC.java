118a229f21cffbe68aee832e35377af81b713358
Federico Corso <fredkors66@gmail.com>
2014-07-10 22:40:39 +0200

    First: read and save xml configuration files

--- src/VASL/build/module/map/QC.java
+++ src/VASL/build/module/map/QC.java
@@ -46,9 +46,36 @@ import java.awt.dnd.DragSourceDragEvent;
 import java.awt.dnd.DragSourceDropEvent;
 import java.awt.dnd.DragSourceEvent;
 import java.awt.dnd.DragSourceListener;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 class QCButton extends JButton
+// <editor-fold>
 {
     QCPieceEntry m_objEntry;
     
@@ -92,8 +119,10 @@ class QCButton extends JButton
         }
     }
 }
+// </editor-fold>
 
 class QCMenuItem extends JMenuItem implements DragSourceListener
+// <editor-fold>
 {
     QCPieceEntry m_objEntry;
     
@@ -162,8 +191,266 @@ class QCMenuItem extends JMenuItem implements DragSourceListener
         DragSource.getDefaultDragSource().removeDragSourceListener(this);
     }
 }
+// </editor-fold>
 
-class QCPieceEntry {
+class QCConfiguration extends DefaultHandler 
+// <editor-fold>
+{
+    private QC m_objQC;
+    private File m_objFile;
+    private String m_strDescription;
+    private ArrayList<QCConfigurationEntry> mar_objListConfigurationEntry;
+    private QCConfigurationEntry m_objCurrentSubMenu;
+    
+    public QCConfiguration(QC objQC, File objFile)
+    {
+        m_objCurrentSubMenu = null;
+        
+        m_objQC = objQC;
+        m_objFile = objFile;
+        
+        m_strDescription = "";
+        mar_objListConfigurationEntry = new ArrayList<QCConfigurationEntry>();
+    }    
+
+    /**
+     * @return the m_objQC
+     */
+    public QC getQC() {
+        return m_objQC;
+    }
+
+    /**
+     * @return the m_objFilePath
+     */
+    public File getFile() {
+        return m_objFile;
+    }
+
+    /**
+     * @return the m_strDescription
+     */
+    public String getDescription() {
+        return m_strDescription;
+    }
+
+    /**
+     * @param m_strDescription the m_strDescription to set
+     */
+    public void setDescription(String m_strDescription) {
+        this.m_strDescription = m_strDescription;
+    }
+
+    /**
+     * @return the mar_objConfigurationEntry
+     */
+    public ArrayList<QCConfigurationEntry> getListConfigurationEntry() {
+        return mar_objListConfigurationEntry;
+    }
+    
+    @Override
+    public void startElement (String uri, String localName,
+                              String qName, Attributes attributes)
+        throws SAXException
+    {
+        if (qName.equalsIgnoreCase("qcconfig")) 
+        {
+            setDescription(attributes.getValue("descr"));
+        }
+        else if (qName.equalsIgnoreCase("qcsubmenu")) 
+        {
+            QCConfigurationEntry l_newEntry = new QCConfigurationEntry(getQC());
+            
+            l_newEntry.setMenu(true);
+            l_newEntry.setPieceSlot(attributes.getValue("slot"));
+            
+            if (m_objCurrentSubMenu != null)
+            {
+                l_newEntry.setParent(m_objCurrentSubMenu);
+                m_objCurrentSubMenu.getListConfigurationEntry().add(l_newEntry);
+            }
+            else
+                mar_objListConfigurationEntry.add(l_newEntry);
+            
+            m_objCurrentSubMenu = l_newEntry;
+        }
+        else if (qName.equalsIgnoreCase("qcentry")) 
+        {
+            QCConfigurationEntry l_newEntry = new QCConfigurationEntry(getQC());
+
+            l_newEntry.setPieceSlot(attributes.getValue("slot"));
+            
+            if (m_objCurrentSubMenu != null)
+                m_objCurrentSubMenu.getListConfigurationEntry().add(l_newEntry);
+            else
+                mar_objListConfigurationEntry.add(l_newEntry);            
+        }
+    }  
+    
+    @Override
+    public void endElement (String uri, String localName, String qName)
+        throws SAXException
+    {
+        if (qName.equalsIgnoreCase("qcsubmenu")) 
+            m_objCurrentSubMenu = m_objCurrentSubMenu.getParent();
+    }  
+    
+    public boolean SaveXML() 
+    {
+        try 
+        {
+            if (m_objFile != null)
+            {
+                if (!m_objFile.exists())
+                    m_objFile.createNewFile();
+
+                DocumentBuilderFactory l_objDocumentBuilderFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder l_objDocumentBuilder = l_objDocumentBuilderFactory.newDocumentBuilder();
+
+                // root elements
+                Document l_objDocument = l_objDocumentBuilder.newDocument();
+                Element l_objRootElement = l_objDocument.createElement("qcconfig");
+
+                Attr l_objAttributes = l_objDocument.createAttribute("descr");
+                l_objAttributes.setValue(getDescription());
+                l_objRootElement.setAttributeNode(l_objAttributes);
+
+                l_objDocument.appendChild(l_objRootElement);
+
+                for (QCConfigurationEntry l_objConfigurationEntry : mar_objListConfigurationEntry)
+                    l_objConfigurationEntry.WriteXML(l_objDocument, l_objRootElement);
+
+                // write the content into xml file
+                TransformerFactory l_objTransformerFactory = TransformerFactory.newInstance();
+                Transformer l_objTransformer = l_objTransformerFactory.newTransformer();
+                DOMSource l_objDOMSource = new DOMSource(l_objDocument);
+
+                l_objTransformer.transform(l_objDOMSource, new StreamResult(new FileOutputStream(m_objFile)));
+            }
+            
+            return true;
+ 
+        } 
+        catch (Exception e) 
+        {
+        }
+
+        return false;
+    }
+}
+// </editor-fold>
+
+class QCConfigurationEntry
+// <editor-fold>
+{
+    private QC m_objQC;
+    private boolean m_bMenu;
+    private ArrayList<QCConfigurationEntry> mar_objListConfigurationEntry;
+    private String m_strPieceSlot;    
+    private QCConfigurationEntry m_objParentEntry;
+    
+    public QCConfigurationEntry(QC objQC)
+    {
+        m_objQC = objQC;
+        m_bMenu = false;
+        mar_objListConfigurationEntry = null;
+        m_strPieceSlot = "";
+        m_objParentEntry = null;
+    }   
+
+    /**
+     * @return the m_bMenu
+     */
+    public boolean isMenu() {
+        return m_bMenu;
+    }
+
+    /**
+     * @param m_bMenu the m_bMenu to set
+     */
+    public void setMenu(boolean m_bMenu) {
+        this.m_bMenu = m_bMenu;
+        
+        if (m_bMenu)
+            mar_objListConfigurationEntry = new ArrayList<QCConfigurationEntry>();
+    }
+
+    /**
+     * @return the mar_objListConfigurationEntry
+     */
+    public ArrayList<QCConfigurationEntry> getListConfigurationEntry() {
+        return mar_objListConfigurationEntry;
+    }
+
+    /**
+     * @return the m_strPieceSlot
+     */
+    public String getPieceSlot() {
+        return m_strPieceSlot;
+    }
+
+    /**
+     * @param m_strPieceSlot the m_strPieceSlot to set
+     */
+    public void setPieceSlot(String m_strPieceSlot) {
+        this.m_strPieceSlot = m_strPieceSlot;
+    }
+
+    /**
+     * @return the m_objQC
+     */
+    public QC getQC() {
+        return m_objQC;
+    }
+
+    /**
+     * @return the m_objParentEntry
+     */
+    public QCConfigurationEntry getParent() {
+        return m_objParentEntry;
+    }
+
+    /**
+     * @param m_objParentEntry the m_objParentEntry to set
+     */
+    public void setParent(QCConfigurationEntry m_objParentEntry) {
+        this.m_objParentEntry = m_objParentEntry;
+    }
+
+    void WriteXML(Document objDocument, Element objElement) 
+    {
+        if (isMenu())
+        {
+            Element l_objEntry = objDocument.createElement("qcsubmenu");
+
+            // set attribute to staff element
+            Attr l_objAttributes = objDocument.createAttribute("slot");
+            l_objAttributes.setValue(getPieceSlot());
+            l_objEntry.setAttributeNode(l_objAttributes);
+            
+            objElement.appendChild(l_objEntry);
+            
+            for (QCConfigurationEntry l_objConfigurationEntry : mar_objListConfigurationEntry)
+                l_objConfigurationEntry.WriteXML(objDocument, l_objEntry);
+        }
+        else
+        {
+            Element l_objEntry = objDocument.createElement("qcentry");
+
+            // set attribute to staff element
+            Attr l_objAttributes = objDocument.createAttribute("slot");
+            l_objAttributes.setValue(getPieceSlot());
+            l_objEntry.setAttributeNode(l_objAttributes);
+            
+            objElement.appendChild(l_objEntry);
+        }
+    }
+}
+// </editor-fold>
+
+class QCPieceEntry 
+// <editor-fold>
+{
     
     public QC m_objParent;
     public String m_strName;
@@ -186,11 +473,128 @@ class QCPieceEntry {
         m_bTopLevel = bTopLevel;
     }
 }
+// </editor-fold>
+
 /**
  * A class to remove all GamePieces with a given name
  */
 public class QC implements Buildable 
 {
+    private final String mc_strDefaultConfig = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+"<qcconfig descr=\"Default configuration\">\n" +
+"	<qcentry slot=\"0\"/>\n" +
+"	<qcentry slot=\"11\"/>\n" +
+"	<qcentry slot=\"12\"/>\n" +
+"	<qcentry slot=\"13\"/>\n" +
+"	<qcentry slot=\"15\"/>\n" +
+"	<qcentry slot=\"18\"/>\n" +
+"	<qcentry slot=\"19\"/>\n" +
+"	<qcentry slot=\"21\"/>\n" +
+"	<qcentry slot=\"1\"/>\n" +
+"	<qcentry slot=\"2\"/>\n" +
+"	<qcentry slot=\"4\"/>\n" +
+"	<qcentry slot=\"63\"/>\n" +
+"	<qcsubmenu slot=\"47\">\n" +
+"		<qcentry slot=\"47\"/>\n" +
+"		<qcentry slot=\"48\"/>\n" +
+"		<qcentry slot=\"49\"/>\n" +
+"		<qcentry slot=\"50\"/>\n" +
+"		<qcentry slot=\"51\"/>\n" +
+"		<qcentry slot=\"52\"/>\n" +
+"	</qcsubmenu>\n" +
+"	<qcsubmenu slot=\"53\">\n" +
+"		<qcentry slot=\"53\"/>\n" +
+"		<qcentry slot=\"54\"/>\n" +
+"		<qcentry slot=\"55\"/>\n" +
+"		<qcentry slot=\"56\"/>\n" +
+"	</qcsubmenu>\n" +
+"	<qcsubmenu slot=\"6\">\n" +
+"		<qcentry slot=\"6\"/>\n" +
+"		<qcentry slot=\"57\"/>\n" +
+"		<qcentry slot=\"59\"/>\n" +
+"		<qcentry slot=\"61\"/>\n" +
+"		<qcentry slot=\"72\"/>\n" +
+"		<qcentry slot=\"58\"/>\n" +
+"		<qcentry slot=\"60\"/>\n" +
+"		<qcentry slot=\"62\"/>\n" +
+"		<qcentry slot=\"114\"/>\n" +
+"	</qcsubmenu>\n" +
+"	<qcentry slot=\"69\"/>\n" +
+"	<qcentry slot=\"146\"/>\n" +
+"	<qcentry slot=\"64\"/>\n" +
+"	<qcsubmenu slot=\"73\">\n" +
+"		<qcentry slot=\"73\"/>\n" +
+"		<qcentry slot=\"74\"/>\n" +
+"		<qcentry slot=\"83\"/>\n" +
+"		<qcentry slot=\"85\"/>\n" +
+"		<qcentry slot=\"88\"/>\n" +
+"	</qcsubmenu>\n" +
+"	<qcsubmenu slot=\"6390\">\n" +
+"		<qcentry slot=\"6390\"/>\n" +
+"		<qcentry slot=\"6391\"/>\n" +
+"	</qcsubmenu>\n" +
+"	<qcsubmenu slot=\"68\">\n" +
+"		<qcentry slot=\"68\"/>\n" +
+"		<qcentry slot=\"66\"/>\n" +
+"		<qcentry slot=\"5903\"/>\n" +
+"		<qcentry slot=\"5902\"/>\n" +
+"	</qcsubmenu>\n" +
+"	<qcsubmenu slot=\"423\">\n" +
+"		<qcentry slot=\"423\"/>\n" +
+"		<qcentry slot=\"773\"/>\n" +
+"		<qcentry slot=\"1066\"/>\n" +
+"		<qcentry slot=\"1589\"/>\n" +
+"		<qcentry slot=\"1939\"/>\n" +
+"		<qcentry slot=\"2122\"/>\n" +
+"		<qcentry slot=\"2353\"/>\n" +
+"		<qcentry slot=\"2831\"/>\n" +
+"		<qcentry slot=\"3192\"/>\n" +
+"		<qcentry slot=\"3380\"/>\n" +
+"		<qcentry slot=\"3573\"/>\n" +
+"		<qcentry slot=\"3634\"/>\n" +
+"		<qcentry slot=\"3916\"/>\n" +
+"	</qcsubmenu>\n" +
+"	<qcsubmenu slot=\"41\">\n" +
+"		<qcentry slot=\"41\"/>\n" +
+"		<qcentry slot=\"5918\"/>\n" +
+"		<qcentry slot=\"5919\"/>\n" +
+"		<qcentry slot=\"5920\"/>\n" +
+"	</qcsubmenu>\n" +
+"	<qcentry slot=\"101\"/>\n" +
+"	<qcentry slot=\"109\"/>\n" +
+"	<qcentry slot=\"111\"/>\n" +
+"	<qcentry slot=\"113\"/>\n" +
+"	<qcentry slot=\"126\"/>\n" +
+"	<qcentry slot=\"128\"/>\n" +
+"	<qcentry slot=\"129\"/>\n" +
+"	<qcentry slot=\"116\"/>\n" +
+"	<qcentry slot=\"123\"/>\n" +
+"	<qcentry slot=\"104\"/>\n" +
+"	<qcentry slot=\"344\"/>\n" +
+"	<qcsubmenu slot=\"41\">\n" +
+"		<qcentry slot=\"41\"/>\n" +
+"		<qcentry slot=\"171\"/>\n" +
+"		<qcentry slot=\"167\"/>\n" +
+"		<qcentry slot=\"169\"/>\n" +
+"		<qcentry slot=\"163\"/>\n" +
+"		<qcentry slot=\"165\"/>\n" +
+"		<qcentry slot=\"202\"/>\n" +
+"		<qcentry slot=\"203\"/>\n" +
+"		<qcentry slot=\"204\"/>\n" +
+"	</qcsubmenu>\n" +
+"	<qcsubmenu slot=\"359\">\n" +
+"		<qcentry slot=\"359\"/>\n" +
+"		<qcentry slot=\"360\"/>\n" +
+"		<qcentry slot=\"361\"/>\n" +
+"		<qcentry slot=\"362\"/>\n" +
+"		<qcentry slot=\"363\"/>\n" +
+"		<qcentry slot=\"364\"/>\n" +
+"		<qcentry slot=\"365\"/>\n" +
+"		<qcentry slot=\"366\"/>\n" +
+"		<qcentry slot=\"367\"/>\n" +
+"	</qcsubmenu>\n" +
+"</qcconfig>";
+    
     private final int c_iModeNormal = 4;
     private final int c_iModeCompact = 2;
     private final int c_iModeUltra = 1;
@@ -200,6 +604,7 @@ public class QC implements Buildable
     private Vector<JPopupMenu> m_PopupMenusV = new Vector<JPopupMenu>();
     private Vector<QCPieceEntry> m_EntriesV = new Vector<QCPieceEntry>();
     private int m_iMode = c_iModeNormal;
+    private ArrayList<QCConfiguration> mar_objListQCConfiguration = new ArrayList<QCConfiguration>();
 
     private void toggleVisibility(JButton objButton) 
     {
@@ -260,6 +665,92 @@ public class QC implements Buildable
         }
     }
 
+    public void loadConfigurations() 
+    {
+        Path l_pathConfigs = Paths.get(Info.getHomeDir() + System.getProperty("file.separator","\\") + "qcconfigs");
+        
+        // TODO Set the default config
+        mar_objListQCConfiguration.clear();
+        
+        // check for configs dir
+        if (Files.notExists(l_pathConfigs)) 
+        {
+            try
+            {
+                Files.createDirectory(l_pathConfigs);
+            } 
+            catch(Exception e)
+            {
+            }        
+        }
+        else // browsing configs files
+        {
+            SAXParser l_objXMLParser = null;
+
+            try 
+            {
+                SAXParserFactory l_objXMLParserFactory = SAXParserFactory.newInstance();
+
+                l_objXMLParser = l_objXMLParserFactory.newSAXParser();
+            } 
+            catch (Exception ex) 
+            {
+                l_objXMLParser = null;                    
+            }
+
+            if (l_objXMLParser != null)
+            {
+                // read default configuration
+                QCConfiguration l_objQCDefaultConfiguration = new QCConfiguration(this, null);
+                
+                try 
+                {
+                    // parse the default configuration
+                    l_objXMLParser.parse(new InputSource(new StringReader(mc_strDefaultConfig)), l_objQCDefaultConfiguration);
+                }
+                catch (Exception ex) 
+                {
+                    l_objQCDefaultConfiguration = null;
+                }
+
+                if (l_objQCDefaultConfiguration != null)
+                    mar_objListQCConfiguration.add(l_objQCDefaultConfiguration);            
+                            
+                File[] lar_objConfigFiles = l_pathConfigs.toFile().listFiles(new FilenameFilter() { public boolean accept(File objFile, String strName) 
+                                                                                    {
+                                                                                        return strName.toLowerCase().endsWith(".xml");
+                                                                                    }});            
+                for (File l_objConfigFile : lar_objConfigFiles) 
+                {
+                    //Create an instance of this class; it defines all the handler methods
+                    QCConfiguration l_objQCConfiguration = new QCConfiguration(this, l_objConfigFile);
+
+                    try 
+                    {
+                        // parse the config file
+                        l_objXMLParser.parse(l_objConfigFile, l_objQCConfiguration);
+                    }
+                    catch (Exception ex) 
+                    {
+                        l_objQCConfiguration = null;
+                    }
+
+                    if (l_objQCConfiguration != null)
+                        mar_objListQCConfiguration.add(l_objQCConfiguration);
+                    else
+                    {
+                        try
+                        {
+                            l_objConfigFile.delete();
+                        }
+                        catch (Exception ex) 
+                        {
+                        }
+                    }
+                }
+            }
+        }      
+    }
     // read the last working mode from file
     public int loadLastWorkingMode() 
     {
@@ -315,6 +806,8 @@ public class QC implements Buildable
 
     public void build(Element e) 
     {	
+        loadConfigurations();
+        
         m_iMode = loadLastWorkingMode();
 
         NodeList l_NodeL = e.getElementsByTagName("*");
