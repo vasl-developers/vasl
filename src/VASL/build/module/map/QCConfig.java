864061b1e4c62cd52d014bbdd71b54779f8451df
Federico Corso <fredkors66@gmail.com>
2014-09-27 23:42:35 +0200

    Implementing tree handling functions

--- src/VASL/build/module/map/QCConfig.java
+++ src/VASL/build/module/map/QCConfig.java
@@ -6,7 +6,6 @@
 
 package VASL.build.module.map;
 
-import VASSAL.build.module.map.PieceMover;
 import VASSAL.tools.image.ImageUtils;
 import VASSAL.tools.imageop.Op;
 import java.awt.Color;
@@ -15,29 +14,27 @@ import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.Toolkit;
-import java.awt.dnd.DnDConstants;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.awt.image.BufferedImage;
 import java.util.Enumeration;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.ExecutionException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
 import javax.swing.JScrollPane;
 import javax.swing.JToolBar;
 import javax.swing.JTree;
+import javax.swing.SwingConstants;
 import javax.swing.plaf.basic.BasicTreeUI;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.MutableTreeNode;
-import javax.swing.tree.TreeCellRenderer;
 import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
@@ -153,6 +150,27 @@ public class QCConfig
         m_bDataModified = false;
     }
     
+    private void SetButtonProperties(JButton objButton, String strImage, String strTooltip, ActionListener objActionListener)
+    {
+        try 
+        {
+            objButton.setIcon(new ImageIcon(Op.load(strImage).getImage(null))); // NOI18N
+        } 
+        catch (Exception ex) 
+        {
+            ex.printStackTrace();
+        }
+        
+        objButton.setToolTipText(strTooltip);
+        objButton.setFocusable(false);
+        objButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        objButton.setText("");
+        objButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        objButton.addActionListener(objActionListener);
+        
+        m_objToolbar.add(objButton);
+    }
+    
     
     /**
      * This method is called from within the constructor to initialize the form.
@@ -166,6 +184,12 @@ public class QCConfig
         m_objToolbar = new JToolBar();
         m_objBtnMoveUp = new JButton();
         m_objBtnMoveDown = new JButton();
+        m_objBtnAddCounter = new JButton();
+        m_objBtnAddMenu = new JButton();
+        m_objBtnSetTitle = new JButton();
+        m_objBtnRemCounter = new JButton();
+        m_objBtnSave = new JButton();
+        m_objBtnExit = new JButton();
         QCSP = new JScrollPane();
         
         m_objModelTree = new DefaultTreeModel(m_objWorkingConfiguration);
@@ -181,85 +205,129 @@ public class QCConfig
         m_objTree.setSelectionPath(l_objRootPath);
         
         m_objToolbar.setFloatable(false);
-        m_objToolbar.setOrientation(javax.swing.SwingConstants.VERTICAL);
+        m_objToolbar.setOrientation(SwingConstants.VERTICAL);
         m_objToolbar.setRollover(true);
-        m_objToolbar.setMaximumSize(new java.awt.Dimension(64, 23));
-        m_objToolbar.setMinimumSize(new java.awt.Dimension(50, 23));
-        m_objToolbar.setPreferredSize(new java.awt.Dimension(50, 23));
-
-        try 
-        {
-            m_objBtnMoveUp.setIcon(new ImageIcon(Op.load("QC/up.png").getImage(null))); // NOI18N
-        } 
-        catch (Exception ex) 
-        {
-            ex.printStackTrace();
-        }
+        m_objToolbar.setMaximumSize(new Dimension(64, 23));
+        m_objToolbar.setMinimumSize(new Dimension(50, 23));
+        m_objToolbar.setPreferredSize(new Dimension(50, 23));
         
-        m_objBtnMoveUp.setToolTipText("Move up the selected node");
-        m_objBtnMoveUp.setFocusable(false);
-        m_objBtnMoveUp.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        m_objBtnMoveUp.setText("");
-        m_objBtnMoveUp.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        m_objBtnMoveUp.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        SetButtonProperties(m_objBtnMoveUp, "QC/up.png", "Move up the selected node", new ActionListener() {
+            public void actionPerformed(ActionEvent evt) 
+            {
                 m_objBtnMoveUpActionPerformed(evt);
             }
         });
-        m_objToolbar.add(m_objBtnMoveUp);
-        
-        try 
-        {
-            m_objBtnMoveDown.setIcon(new ImageIcon(Op.load("QC/down.png").getImage(null))); // NOI18N
-        } 
-        catch (Exception ex) 
-        {
-            ex.printStackTrace();
-        }
-        
-        m_objBtnMoveDown.setToolTipText("Move down the selected node");
-        m_objBtnMoveDown.setFocusable(false);
-        m_objBtnMoveDown.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        m_objBtnMoveDown.setText("");
-        m_objBtnMoveDown.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        m_objBtnMoveDown.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                m_objButtonDownActionPerformed(evt);
+
+        SetButtonProperties(m_objBtnMoveDown, "QC/down.png", "Move down the selected node", new ActionListener() {
+            public void actionPerformed(ActionEvent evt) 
+            {
+                m_objBtnMoveDownActionPerformed(evt);
             }
         });
-        m_objToolbar.add(m_objBtnMoveDown);
+        
+        SetButtonProperties(m_objBtnAddCounter, "QC/add.png", "Add a new button to the toolbar", new ActionListener() {
+            public void actionPerformed(ActionEvent evt) 
+            {
+                m_objBtnAddCounterActionPerformed(evt);
+            }
+        });
+        
+        SetButtonProperties(m_objBtnAddMenu, "QC/submenu.png", "Add a new menu/submenu to the toolbar", new ActionListener() {
+            public void actionPerformed(ActionEvent evt) 
+            {
+                m_objBtnAddMenuActionPerformed(evt);
+            }
+        });
+        
+        SetButtonProperties(m_objBtnSetTitle, "QC/title.png", "Edit the title of a menu or the description of the whole configuration", new ActionListener() {
+            public void actionPerformed(ActionEvent evt) 
+            {
+                m_objBtnSetTitleActionPerformed(evt);
+            }
+        });
+        
+        SetButtonProperties(m_objBtnRemCounter, "QC/delnode.png", "Remove the selected node from the toolbar", new ActionListener() {
+            public void actionPerformed(ActionEvent evt) 
+            {
+                m_objBtnRemCounterActionPerformed(evt);
+            }
+        });
+        
+        SetButtonProperties(m_objBtnSave, "QC/save.png", "Save the configuration and apply the changes", new ActionListener() {
+            public void actionPerformed(ActionEvent evt) 
+            {
+                m_objBtnSaveActionPerformed(evt);
+            }
+        });
+        
+        SetButtonProperties(m_objBtnExit, "QC/exit.png", "Exit the window and discard the unsaved changes", new ActionListener() {
+            public void actionPerformed(ActionEvent evt) 
+            {
+                m_objBtnExitActionPerformed(evt);
+            }
+        });
         
         QCSP.setName(""); // NOI18N
-        QCSP.setPreferredSize(new java.awt.Dimension(300, 500));
+        QCSP.setPreferredSize(new Dimension(300, 500));
         QCSP.setRequestFocusEnabled(false);
 
         m_objTree.setShowsRootHandles(true);
         m_objTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
         QCSP.setViewportView(m_objTree);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(m_objFrame.getContentPane());
+        GroupLayout layout = new GroupLayout(m_objFrame.getContentPane());
         m_objFrame.getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(QCSP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(m_objToolbar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(QCSP, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(m_objToolbar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(m_objToolbar, javax.swing.GroupLayout.DEFAULT_SIZE, 750, Short.MAX_VALUE)
-            .addComponent(QCSP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addComponent(m_objToolbar, GroupLayout.DEFAULT_SIZE, 750, Short.MAX_VALUE)
+            .addComponent(QCSP, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         QCSP.getAccessibleContext().setAccessibleName("");
 
         m_objFrame.pack();
     }    
-    private void m_objButtonDownActionPerformed(java.awt.event.ActionEvent evt) {                                                
-        // TODO add your handling code here:
+    private void m_objBtnMoveDownActionPerformed(ActionEvent evt) 
+    {
+        TreePath l_objTreePath = m_objTree.getSelectionPath();        // get path of selected node.
+
+        if (l_objTreePath == null) 
+            return;
+
+        boolean l_bIsExpanded = m_objTree.isExpanded(l_objTreePath);
+
+        DefaultMutableTreeNode l_objNode = (DefaultMutableTreeNode) l_objTreePath.getLastPathComponent();
+        DefaultMutableTreeNode l_objParentNode = (DefaultMutableTreeNode) l_objNode.getParent();
+
+        //now get the index of the selected node in the DefaultTreeModel
+        int l_iIndex = m_objModelTree.getIndexOfChild(l_objParentNode, l_objNode);
+        int l_iMaxIndex = m_objModelTree.getChildCount(l_objParentNode) - 1;
+
+        // if selected node is first, return (can't move it up)
+        if (l_iIndex < l_iMaxIndex) 
+        {
+            m_objModelTree.removeNodeFromParent(l_objNode);
+            m_objModelTree.insertNodeInto(l_objNode, l_objParentNode, l_iIndex + 1);    // move the node
+
+            TreePath l_objNodePath = new TreePath(l_objNode.getPath());
+
+            if (l_bIsExpanded) 
+                m_objTree.expandPath(l_objNodePath);
+
+            m_objTree.scrollPathToVisible(l_objNodePath);
+            m_objTree.setSelectionPath(l_objNodePath);
+        
+            setDataModified(true);
+        }
     }                                               
 
-    private void m_objBtnMoveUpActionPerformed(java.awt.event.ActionEvent evt) 
+    private void m_objBtnMoveUpActionPerformed(ActionEvent evt) 
     {
         TreePath l_objTreePath = m_objTree.getSelectionPath();        // get path of selected node.
 
@@ -288,9 +356,102 @@ public class QCConfig
             m_objTree.scrollPathToVisible(l_objNodePath);
             m_objTree.setSelectionPath(l_objNodePath);
         
-            m_bDataModified = true;
+            setDataModified(true);
         }
     }                                              
+    
+    private void m_objBtnAddCounterActionPerformed(ActionEvent evt) 
+    {
+        JOptionPane.showMessageDialog(self().m_objFrame, 
+            "To add a button to the toolbar you must simply do a drag & drop from the VASL counters window to the tree." 
+                + "\nThe new button will be inserted after the currently selected node (if there is no node selected or if the selected \nnode represents "
+                + "a submenu, it will be added as last child).", 
+            "To add a button to the toolbar", 
+            INFORMATION_MESSAGE);
+    }
+    
+    private void m_objBtnAddMenuActionPerformed(ActionEvent evt) 
+    {
+    }
+    
+    private void m_objBtnSetTitleActionPerformed(ActionEvent evt) 
+    {
+    }
+    
+    private void m_objBtnRemCounterActionPerformed(ActionEvent evt) 
+    {
+        int l_iSelectedOption = JOptionPane.YES_OPTION;
+        TreePath l_objTreePath = m_objTree.getSelectionPath();        // get path of selected node.
+
+        if (l_objTreePath == null) 
+            return;
+
+        DefaultMutableTreeNode l_objNode = (DefaultMutableTreeNode) l_objTreePath.getLastPathComponent();
+        DefaultMutableTreeNode l_objParentNode = (DefaultMutableTreeNode) l_objNode.getParent();
+        
+        if (l_objNode.isRoot())
+            return;
+        
+        if ((((QCConfigurationEntry)l_objNode).isMenu()) && (m_objModelTree.getChildCount(l_objNode) > 0))
+        {
+            l_iSelectedOption = JOptionPane.showConfirmDialog(self().m_objFrame, 
+                                              "Are you sure? All the child items also will be removed...", 
+                                              "Do you want to remove this menu?", 
+                                              JOptionPane.YES_NO_OPTION,
+                                              JOptionPane.QUESTION_MESSAGE);
+        }
+        
+        if (l_iSelectedOption == JOptionPane.YES_OPTION)
+        {
+            int l_iIndex = m_objModelTree.getIndexOfChild(l_objParentNode, l_objNode);
+            
+            m_objModelTree.removeNodeFromParent(l_objNode);            
+            
+            while (true)
+            {
+                DefaultMutableTreeNode l_objSelectedNode = null;
+                
+                if (l_iIndex < m_objModelTree.getChildCount(l_objParentNode))
+                    l_objSelectedNode = (DefaultMutableTreeNode) m_objModelTree.getChild(l_objParentNode, l_iIndex);
+                
+                if (l_objSelectedNode != null)
+                {
+                    TreePath l_objNodePath = new TreePath(l_objSelectedNode.getPath());
+
+                    m_objTree.scrollPathToVisible(l_objNodePath);
+                    m_objTree.setSelectionPath(l_objNodePath);
+
+                    break;
+                }
+                else
+                {
+                    l_iIndex--;
+                   
+                    if (l_iIndex < 0)
+                    {
+                        TreePath l_objNodePath = new TreePath(l_objParentNode.getPath());
+
+                        m_objTree.scrollPathToVisible(l_objNodePath);
+                        m_objTree.setSelectionPath(l_objNodePath);
+                       
+                        break;
+                    }
+                }
+            }
+            
+            setDataModified(true);
+        }
+    }
+    
+    private void m_objBtnSaveActionPerformed(ActionEvent evt) 
+    {
+        
+    }
+    
+    private void m_objBtnExitActionPerformed(ActionEvent evt) 
+    {
+        m_objFrame.dispatchEvent(new WindowEvent(m_objFrame, WindowEvent.WINDOW_CLOSING));         
+    }
     
     public void expandAll(JTree objTree) 
     {
@@ -317,11 +478,17 @@ public class QCConfig
     }
     
     // Variables declaration - do not modify                     
-    private javax.swing.JToolBar m_objToolbar;
-    private javax.swing.JScrollPane QCSP;
-    private javax.swing.JTree m_objTree;
-    private javax.swing.JButton m_objBtnMoveUp;
-    private javax.swing.JButton m_objBtnMoveDown;
+    private JToolBar m_objToolbar;
+    private JScrollPane QCSP;
+    private JTree m_objTree;
+    private JButton m_objBtnMoveUp;
+    private JButton m_objBtnMoveDown;
+    private JButton m_objBtnAddCounter;
+    private JButton m_objBtnAddMenu;
+    private JButton m_objBtnSetTitle;
+    private JButton m_objBtnRemCounter;
+    private JButton m_objBtnSave;
+    private JButton m_objBtnExit;
     // End of variables declaration                   
 
     /**
@@ -368,7 +535,7 @@ public class QCConfig
                 {
                     int l_iSelectedOption = JOptionPane.YES_OPTION;
                     
-                    if (m_bDataModified)
+                    if (isDataModified())
                     {
                         l_iSelectedOption = JOptionPane.showConfirmDialog(self().m_objFrame, 
                                                           "Are you sure? Any changes not saved will be lost...", 
@@ -406,8 +573,9 @@ public class QCConfig
         }
         
         m_objFrame.setTitle("Editing : " + m_objWorkingConfiguration.getDescription());
-        m_objFrame.setVisible(true);                    
-        m_bDataModified = false;
+        m_objFrame.setVisible(true);
+        
+        setDataModified(false);
     }
 
     /**
@@ -415,5 +583,25 @@ public class QCConfig
      */
     public QCConfig self() {
         return m_objSelf;
+    }
+
+    /**
+     * @return the m_bDataModified
+     */
+    public boolean isDataModified() {
+        return m_bDataModified;
+    }
+
+    /**
+     * @param m_bDataModified the m_bDataModified to set
+     */
+    public void setDataModified(boolean m_bDataModified) {
+        this.m_bDataModified = m_bDataModified;
+        EnableButtons();
+    }
+
+    private void EnableButtons() 
+    {
+        
     }
 }
