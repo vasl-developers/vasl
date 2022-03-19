a910e49fe6b0801ffaccbcb44045cf8e0df7fa7b
Doug Rimmer <dougrimmer@rogers.com>
2022-03-19 15:07:43 -0400

    minor cropping bug fix

--- src/VASL/build/module/map/boardPicker/ASLBoard.java
+++ src/VASL/build/module/map/boardPicker/ASLBoard.java
@@ -247,7 +247,11 @@ public class ASLBoard extends Board {
         Rectangle newCropBounds = new Rectangle(0, 0, -1, -1);
         newCropBounds.x = (row1.length() == 0 ? 0 : getGrid().getLocation(row1 + "0").x);
         newCropBounds.width = row2.length() == 0 ? -1 : (getGrid().getLocation(row2 + "0").x - newCropBounds.x);
-        newCropBounds.y = coord1.length() == 0 ? 0 : (getGrid().getLocation("a" + coord1).y - (int) (dy / 2));
+        newCropBounds.y = coord1.length() == 0 ? 0 : (getGrid().getLocation("a" + coord1). y - (int) (dy / 2));
+        // hack to fix problem with RB cropping
+        if (dy == 64.47 || dy ==64.4528){
+            newCropBounds.y += Math.ceil((double) Integer.valueOf(coord1)/10);
+        }
         newCropBounds.height = coord2.length() == 0 ? -1 : (getGrid().getLocation("a" + coord2).y + (int) (dy / 2) - newCropBounds.y);
         if (nearestFullRow) {
             if (newCropBounds.width > 0 && Math.abs(newCropBounds.x + newCropBounds.width - uncroppedSize.width) > dx / 4) {
