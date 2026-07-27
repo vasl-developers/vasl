d314254742320734e5d0748fd1146df758df9360
Doug Rimmer <dougerimmer@gmail.com>
2026-07-27 16:45:18 -0400

    #2641 nvr works with los

---
+++ src/VASL/Gamedata/IllumGunFlashDataSet.java
@@ -0,0 +1,169 @@
+package VASL.Gamedata;
+
+import VASL.LOS.counters.CounterMetadata;
+import VASSAL.build.GameModule;
+import VASSAL.tools.DataArchive;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedHashMap;
+
+/**
+ * This class provides access to the Illumination and Gunflash counter metadata file in the module archive
+ */
+public class IllumGunFlashDataSet implements StandardXMLDataSet {
+
+    // name of the counter metadata file in the module archive
+    private final static String illgunMetadataFileName = "IlluminationCounterMetadata.xml";
+
+    // XML element and attribute names
+    protected static final String illuminationMetadataElement = "illuminationCounterMetadata";
+    protected static final String starshellCounterElement = "starshell";
+    protected static final String illumRoundCounterElement = "illumround";
+    protected static final String blazeCounterElement = "blaze";
+    protected static final String flameCounterElement = "flame";
+    protected static final String tripflaresCounterElement = "tripflares";
+    protected static final String searchlightsCounterElement = "searchlights";
+
+    protected static final String counterNameAttribute = "name";
+    protected static final String counterHexAttribute = "hex";
+    protected static final String counterRangeAttribute = "range";
+    protected static final String counterZoneAttribute = "zone";
+
+
+    // List of the counter elements
+    protected LinkedHashMap<String, IllumGunFlashMetadata> metadataElements = new LinkedHashMap<String, IllumGunFlashMetadata>(30);
+
+    public IllumGunFlashDataSet(){
+
+        DataArchive archive = GameModule.getGameModule().getDataArchive();
+        try (InputStream inputStream = archive.getInputStream(illgunMetadataFileName)) {
+
+            // counter metadata
+            parseIllgunCounterMetadataFile(inputStream);
+
+            // give up on any errors
+        }
+        catch (IOException e) {
+            metadataElements = null;
+        }
+        catch (JDOMException e) {
+            metadataElements = null;
+        }
+        catch (NullPointerException e) {
+            metadataElements = null;
+        }
+    }
+
+        /**
+         * Parses the counter metadata file
+         * @param metadata an <code>InputStream</code> for the counter metadata XML file
+         * @throws org.jdom2.JDOMException
+         */
+        public void parseIllgunCounterMetadataFile(InputStream metadata) throws JDOMException {
+
+            SAXBuilder parser = new SAXBuilder();
+
+            try {
+
+                // the root element will be the counter metadata element
+                Document doc = parser.build(metadata);
+                Element root = doc.getRootElement();
+
+                // read the counters
+                if(root.getName().equals(illuminationMetadataElement)) {
+
+                    parseCounters(root);
+                }
+
+            }
+            catch (IOException e) {
+                e.printStackTrace(System.err);
+                throw new JDOMException("Error reading the counter metadata", e);
+            }
+        }
+
+        /**
+         * Parses the counter metadata element
+         * @param element the counter metadata element
+         * @throws org.jdom2.JDOMException
+         */
+        protected void parseCounters(Element element) throws JDOMException {
+
+            // make sure we have the right element
+            assertElementName(element, illuminationMetadataElement);
+
+            for(Element e: element.getChildren()) {
+
+                IllumGunFlashMetadata illumgunflashMetadata = null;
+                String name = e.getAttributeValue(counterNameAttribute);
+
+                // ignore any child elements that are not counter rules
+                if(e.getName().equals(starshellCounterElement)) {
+                    // read the attributes of the element
+                    illumgunflashMetadata = new IllumGunFlashMetadata(name, IllumGunFlashMetadata.CounterType.STARSHELL);
+                    illumgunflashMetadata.setHeight(e.getAttribute(counterHeightAttribute).getIntValue());
+                    illumgunflashMetadata.setHindrance(e.getAttribute(counterHindranceAttribute).getIntValue());
+
+                }
+                else if(e.getName().equals(illumRoundCounterElement)) {
+                    illumgunflashMetadata = new IllumGunFlashMetadata(name, IllumGunFlashMetadata.CounterType.ILLUMROUND);
+                    illumgunflashMetadata.setTerrain(e.getAttributeValue(counterTerrainAttribute));
+                    if (e.getAttribute(counterLevelAttribute) != null) {
+                        illumgunflashMetadata.setLevel(e.getAttribute(counterLevelAttribute).getIntValue());
+                    }
+                }
+                else if(e.getName().equals(flameCounterElement)) {
+                    illumgunflashMetadata = new IllumGunFlashMetadata(name, IllumGunFlashMetadata.CounterType.FLAME);
+                    illumgunflashMetadata.setTerrain(e.getAttributeValue(counterTerrainAttribute));
+                    if (e.getAttribute(counterCoveredArchAttribute) != null) {
+                        illumgunflashMetadata.setCoverArch(e.getAttribute(counterCoveredArchAttribute).getIntValue());
+                    }
+                    if (e.getAttribute(counterHexsideAttribute) != null) {
+                        illumgunflashMetadata.setHexside(e.getAttribute(counterHexsideAttribute).getIntValue());
+                    }
+                }
+                else if(e.getName().equals(blazeCounterElement)) {
+                    illumgunflashMetadata = new IllumGunFlashMetadata(name, IllumGunFlashMetadata.CounterType.BLAZE);
+                    illumgunflashMetadata.setRotation(e.getAttribute(counterRotationAttribute).getIntValue());
+                    illumgunflashMetadata.setIsBarrage(e.getAttribute(counterIsBarrageAttribute).getBooleanValue());
+                }
+                else if(e.getName().equals(tripflaresCounterElement)) {
+                    illumgunflashMetadata = new IllumGunFlashMetadata(name, IllumGunFlashMetadata.CounterType.TRIPFLARES);
+                }
+                else if(e.getName().equals(searchlightsCounterElement)) {
+                    illumgunflashMetadata = new IllumGunFlashMetadata(name, IllumGunFlashMetadata.CounterType.SEARCHLIGHTS);
+                }
+
+
+                metadataElements.put(name, illumgunflashMetadata);
+            }
+        }
+
+        /**
+         * @return the list of LOS counter rules
+         */
+        public LinkedHashMap<String, IllumGunFlashMetadata> getMetadataElements(){
+            return metadataElements;
+        }
+
+        /**
+         * Assert the element has the given name otherwise throw an exception
+         * @param element the element
+         * @param elementName the element name
+         * @throws org.jdom2.JDOMException
+         */
+        private void assertElementName(Element element, String elementName) throws JDOMException {
+
+            // make sure we have the right element
+            if(!element.getName().equals(elementName)) {
+                throw new JDOMException("Invalid element passed to an element parser: " + elementName);
+            }
+        }
+
+    }
+
---
+++ src/VASL/Gamedata/IllumGunFlashMetadata.java
@@ -0,0 +1,183 @@
+package VASL.Gamedata;
+
+public class IllumGunFlashMetadata {
+
+
+
+        /*
+            name = game piece name (i.e. piece.getName())
+            hindrance = # of hindrances/hex
+            height = height of LOS hindrance
+            terrain = map terrain type
+            level - denotes location level
+            position - for location denotes if pieces above/below the counter are in the location.
+            coverArch - covered arch of location
+            rotation - used only for Barrage, rotation of Barrage counter
+            isBarrange - used for OBA/Barrage
+
+         */
+        private String name;
+        private String terrain;
+        private int height;
+        private int hindrance;
+        private VASL.LOS.counters.CounterMetadata.CounterType type;
+        private int level;
+        private String position;
+        private int coverArch;
+        private int rotation;
+        private boolean isBarrage;
+        private int hside;
+
+        public static enum CounterType {STARSHELL, ILLUMROUND, FLAME, BLAZE, TRIPFLARES, SEARCHLIGHTS}
+
+        public IllumGunFlashMetadata(String name, VASL.Gamedata.IllumGunFlashMetadata.CounterType type) {
+            this.name = name;
+            this.type = type;
+        }
+
+        /**
+         * @return the counter name
+         */
+        public String getName() {
+            return name;
+        }
+
+        /**
+         * @return the terrain name (for terrain-type counters)
+         */
+        public String getTerrain() {
+            return terrain;
+        }
+
+        /**
+         * @return the height (for smoke-type counters)
+         */
+        public int getHeight() {
+            return height;
+        }
+
+        /**
+         * @return the hindrance amount (for smoke counters)
+         */
+        public int getHindrance() {
+            return hindrance;
+        }
+
+        /**
+         * @return the hexside amount (for Rowhouse counters)
+         */
+        public int getHexside() {
+            return hside;
+        }
+        /**
+         * @return the counter type
+         */
+        public VASL.LOS.counters.CounterMetadata.CounterType getType() {
+            return type;
+        }
+
+        /**
+         * Set the terrain (for terrain-type counters)
+         * @param terrain the terrain name
+         */
+        public void setTerrain(String terrain) {
+            this.terrain = terrain;
+        }
+
+        /**
+         * Set the smoke height (for smoke-type counter)
+         * @param height the smoke height
+         */
+        public void setHeight(int height) {
+            this.height = height;
+        }
+
+        /**
+         * Set the smoke hindrance (for smoke-type counters)
+         * @param hindrance the smoke hindrance level
+         */
+        public void setHindrance(int hindrance) {
+            this.hindrance = hindrance;
+        }
+
+        /**
+         * Set the hexside
+         * @param hside the hexside
+         */
+        public void setHexside(int hexside) {
+            this.hside = hexside;
+        }
+        /**
+         * @return the location level
+         */
+        public int getLevel() {
+            return level;
+        }
+
+        /**
+         * Set the location level
+         * @param level the level
+         */
+        public void setLevel(int level) {
+            this.level = level;
+        }
+
+        /**
+         * @return the piece covered arch
+         */
+        public int getCoverArch() {
+            return coverArch -1;
+        }  //counters use 1-6, code uses 0-5
+
+        /**
+         * Set the piece covered arch
+         * @param coverArch the covered arch
+         */
+        public void setCoverArch(int coverArch) {
+            this.coverArch = coverArch;
+        }
+
+        /**
+         * @return the location position
+         */
+        public String getPosition() {
+            return position;
+        }
+
+        /**
+         * Set the location position
+         * @param position the covered arch
+         */
+        public void setPosition(String position) {
+            this.position = position;
+        }
+
+        /**
+         * @return the rotation for Barrage/OBA
+         */
+        public int getRotation() {
+            return rotation;
+        }
+
+        /**
+         * Set the rotation for Barrage/OBA
+         */
+        public void setRotation(int rotation) {
+            this.rotation = rotation;
+        }
+
+        /**
+         * @return is Barrage
+         */
+        public boolean getIsBarrage() {
+            return isBarrage;
+        }
+
+        /**
+         * Set is Barrage
+         */
+        public void setIsBarrage(boolean isBarrage) {
+            this.isBarrage = isBarrage;
+        }
+
+    }
